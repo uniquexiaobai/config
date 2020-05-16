f9e72b6f8432116d7bc6158a72610860cf4532b8
uniquexiaobai <uniquexiaobai@gmail.com>
2020-05-16 23:20:02 +0800

    add eslint editorconfig

---
+++ eslint/.eslintrc.js
@@ -0,0 +1,30 @@
+// 参考：https://blog.geographer.fr/eslint-guide
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/forbid-prop-types': [0, { forbid: ['any'] }],
+    'react/prop-types': 0,
+    'no-console': 'off',
+  },
+};
